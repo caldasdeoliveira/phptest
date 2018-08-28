02d504e3cf282edfb69830fa0586103fe7b9fea4
caldasdeoliveira <diogocaldasdeoliveira@gmail.com>
2018-08-28 17:03:14 +0100

    add test file

---
+++ test.php
@@ -0,0 +1,9 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+   #фдргчрфгчччдсаагфадчсфгчфсгфсгсдфгредфгггррдсафдфчвб мн,бннбмбн бнм
+ <?php echo '<p>Hello World</p>'; ?>
+ </body>
+</html>
